e2ed1b6fc1de7a450e2b3272dece4c422804e55e
ArakiTakaki <sjyyj008@gmail.com>
2018-03-18 13:31:13 +0900

    初期設定１

---
+++ router/router.go
@@ -0,0 +1,26 @@
+package router
+
+import (
+	context "github.com/ArakiTakaki/Context"
+	"github.com/gin-gonic/gin"
+)
+
+func GetRouter() *gin.Engine {
+	var r = gin.Default()
+	context.New()
+
+	r.Static("/js", "./public/js")
+	r.Static("/css", "./public/css")
+	r.Static("/image", "./public/image")
+	r.Static("/template", "./public/template")
+
+	r.LoadHTMLGlob("views/*")
+
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(400, "404.html", nil)
+	})
+
+	//ルーティンググループテンプレート
+
+	return r
+}
