ce652308b22df0ddb347189c7a3a29c8c325fa8a
ArakiTakaki <sjyyj008@gmail.com>
2018-03-28 21:23:27 +0900

    ?

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"github.com/ArakiTakaki/singlePageLayoutLesson/models"
 	"github.com/ArakiTakaki/singlePageLayoutLesson/router"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
-	// db := models.Get()
-	// defer db.Close()
-	// models.CreateTables()
-	// models.GetAuthor()
-
+	go def()
 	var r = router.GetRouter()
 	r.Run(":8000")
+}
+
+func def() {
+	db := models.Get()
+	defer db.Close()
+	models.GetAuthor()
 }
--- models/query.go
+++ models/query.go
@@ -8,14 +8,15 @@ import (
 
 // GetAuthor 記者一覧を返却するメソッド
 func GetAuthor() interface{} {
-	value, err := context.Get("Author")
+	sql, err := context.Get("Author")
 	if err != nil {
 		db := Get()
 		defer db.Close()
 		fmt.Println("DBから記述者情報をゲットしました。")
 		query := db.Find(&Author{})
+		fmt.Println(query)
 		context.Put("Author", query.Value)
 		return query.Value
 	}
-	return value
+	return sql
 }
